3d92a083257f66e177aa66fed87ff650f5e64987
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-08-20 13:05:14 +0900

    [update]GameManagerの機能更新

--- Assets/Goto/Scripts/GameManager.cs
+++ Assets/Goto/Scripts/GameManager.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] float _timer = 60;
+    /// <summary>制限時間用の時間を測るタイマー</summary>
+    [SerializeField, Header("制限時間用の時間を測るタイマー"), Tooltip("制限時間用の時間を測るタイマー")] float _timer = 60f;
 
     public static GameManager _instance;
+    /// <summary>スコア用変数</summary>
     int _score = 0;
+    /// <summary>ゲームスタートの判定</summary>
+    public bool _startGame = false;
 
     // Start is called before the first frame update
     void Start()
@@ -13,7 +17,7 @@ public class GameManager : MonoBehaviour
         //GameManagerが既にあるかを判定する
         if (_instance == null)
         {
-            // このオブジェクトはシーンをまたいでも消されないs
+            // このオブジェクトはシーンをまたいでも消されない
             _instance = this;
         }
         else
@@ -26,20 +30,31 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _timer -= Time.deltaTime;
+        // ゲームがスタートしたときのみ測る
+        if (_startGame) 
+        {
+            _timer -= Time.deltaTime;
+        }
     }
 
+    /// <summary>制限時間用の時間を測るタイマー</summary>
     public float Timer
     {
         get { return _timer; }
         set { _timer = value; }
     }
 
+    /// <summary>スコア用変数</summary>
     public int Score
     {
         get { return _score; }
         set { _score = value; }
     }
 
-    
+    /// <summary>ゲームスタートの判定</summary>
+    public bool StartGame
+    {
+        get { return _startGame; }
+        set { _startGame = value; }
+    }
 }
