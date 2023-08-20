3fe8d0483cc58e5109e4cc45de4f38480b77d287
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-08-20 17:49:11 +0900

    [update]スコア機能の更新

--- Assets/Yoshida/ScoreTextManager.cs
+++ Assets/Yoshida/ScoreTextManager.cs
@@ -5,13 +5,14 @@ public class ScoreTextManager : MonoBehaviour
 {
     [SerializeField] int _myScore = 0;
     [SerializeField] int _bestScore = 0;
-    [SerializeField] Text _myScoreText;
-    [SerializeField] Text _bestScoreText;
-
+    Text _myScoreText;
+    Text _bestScoreText;
     void Start()
     {
         _myScore = PlayerPrefs.GetInt("MyScore", _myScore);
         _bestScore = PlayerPrefs.GetInt("BestScore", _bestScore);
+        _myScoreText = GetComponent<Text>();
+        _bestScoreText = GetComponent<Text>();
         _myScoreText.text = _myScore.ToString();
         _bestScoreText.text = _bestScore.ToString();
     }
