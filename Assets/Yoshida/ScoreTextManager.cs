9bc1aaf0aefbbbde6120ddfbe0d70c08cc14edc4
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-08-20 17:51:35 +0900

    [update]スコアのセーブ機能の更新

--- Assets/Yoshida/ScoreTextManager.cs
+++ Assets/Yoshida/ScoreTextManager.cs
@@ -5,12 +5,13 @@ public class ScoreTextManager : MonoBehaviour
 {
     [SerializeField] int _myScore = 0;
     [SerializeField] int _bestScore = 0;
-    Text _myScoreText;
-    Text _bestScoreText;
+    [SerializeField] Text _myScoreText;
+    [SerializeField] Text _bestScoreText;
+    
     void Start()
     {
-        _myScore = PlayerPrefs.GetInt("MyScore", _myScore);
-        _bestScore = PlayerPrefs.GetInt("BestScore", _bestScore);
+        _myScore = PlayerPrefs.GetInt("MyScore");
+        _bestScore = PlayerPrefs.GetInt("BestScore");
         _myScoreText = GetComponent<Text>();
         _bestScoreText = GetComponent<Text>();
         _myScoreText.text = _myScore.ToString();
