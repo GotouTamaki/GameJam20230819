d7257e2704717b241b29e9a4722adcfa8777d13c
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-08-20 11:57:03 +0900

    [add]籠と団扇の新しいプレハブの作成 [update]仮の合わせ [update]羽などの細かい調整

--- Assets/Goto/Scripts/KazoManager.cs
+++ Assets/Goto/Scripts/KazoManager.cs
@@ -1,25 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class BirdController : MonoBehaviour
+public class KazoManager : MonoBehaviour
 {
-    [SerializeField] float _moveSpeed = 1.0f;
     [SerializeField] float _telepotePosition = 0.0f;
-
-    Rigidbody _rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rb.AddForce(_moveSpeed, 0, 0, ForceMode.Force);
+        
     }
 
     private void OnTriggerEnter(Collider other)
